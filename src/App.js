a9b4b357f6e875c0925ea892328b14cbac101a82
Viktor Dimitrov <Viktor.Dimitrov@bulpros.com>
2019-12-12 18:38:22 +0200

    added travis file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Uchi be! Uchi!
+          learn react
         </a>
       </header>
     </div>
