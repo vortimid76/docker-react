41305e02deb41dc77313ff35e7b09b50026713c7
Viktor Dimitrov <Viktor.Dimitrov@bulpros.com>
2019-12-12 17:36:57 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  This was modifyed for test!
+	  Stiga se mota, Uchi!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Uchi be! Uchi!
         </a>
       </header>
     </div>
